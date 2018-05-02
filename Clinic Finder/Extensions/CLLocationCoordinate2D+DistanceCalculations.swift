ba71e2a830ac0a153357e4447a3457c815781aa8
Hayek, Omar <Omar.Hayek@ncr.com>
2018-05-02 00:27:40 -0400

    added small extension

---
+++ Clinic Finder/Extensions/CLLocationCoordinate2D+DistanceCalculations.swift
@@ -0,0 +1,24 @@
+//
+//  CLLocationCoordinate2D+DistanceCalculations.swift
+//  Clinic Finder
+//
+//  Created by Hayek, Omar on 5/2/18.
+//  Copyright © 2018 Hayek, Omar. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+extension CLLocationCoordinate2D {
+    
+//    func distanceFromUserLocation() -> CLLocationDistance {
+//        
+//    }
+    
+    // distance in meters, as explained in CLLoactionDistance definition
+    func distance(from: CLLocationCoordinate2D) -> CLLocationDistance {
+        let destination = CLLocation(latitude:from.latitude,longitude:from.longitude)
+        return CLLocation(latitude: latitude, longitude: longitude).distance(from: destination)
+    }
+    
+}
