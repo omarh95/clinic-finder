55faa310b0312204f1ca4ba342ab3312d2d86b6c
Hayek, Omar <Omar.Hayek@ncr.com>
2018-05-09 11:25:11 -0400

    more info vc init

---
+++ Clinic Finder/View Controllers/MoreInformationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoreInformationViewController.swift
+//  Clinic Finder
+//
+//  Created by Hayek, Omar on 5/8/18.
+//  Copyright © 2018 Hayek, Omar. All rights reserved.
+//
+
+import UIKit
+
+class MoreInformationViewController: UIViewController {
+
+    @IBOutlet var testLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        testLabel.text = "asdasdasd"
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
